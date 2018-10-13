f5c4d8cc2485b2f21a3c0316da4ef5cf69e4a39f
Nilansh Bansal <bansalnilansh@gmail.com>
2018-10-13 09:45:30 +0530

    merging mailer code and adding packages

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ var bodyParser = require('body-parser');
 var email_validator = require('email-validator');
 var password_validator = require('password-validator');
 var bcrypt = require("bcrypt");
-
+var randomstring = require("randomstring");
+var nodemailer = require("nodemailer");
 var port = 8080;
 
+var from_email = 'jatinjain2775@gmail.com';
+var from_pass = 'jatinj@2775';
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: from_email,
+        pass: from_pass
+    }
+});
 
 var schema = new password_validator();
 schema
@@ -43,6 +53,30 @@ app.post('/signup', function(req, res){
     var password = req.body.password
     var hash_pass = bcrypt.hashSync(password, 10);
     if(!verifyCredentials(firstName, lastName, email, password)){
+        res.render("Invalid");
+    }else{
+        var verification_token = randomstring.generate();
+        var url = "http://localhost:8080/"+email+"/"+verification_token;
+        var mailOptions = {
+            from: from_email,
+            to: email,
+            subject: 'Verify your account',
+            text:'<a href='+url+'>'+url+'</a>'
+        }
+        var query = "Insert into users(first_name, last_name, email, password, verification_token) values ('"+firstName+"','"+lastName+"','"+email+"','"+hash_pass+"','"+verification_token+"')";
+        con.query(query, function(err, result){
+            if(err){
+                res.end("Something went wrong");
+            }else{
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        res.end("Something went wrong");
+                    }else{
+                      res.end("Mail sent successfully");  
+                    }
+                });
+            }
+        });
         
     }
 
@@ -51,8 +85,28 @@ app.post('/signup', function(req, res){
 app.post('/login', function(req, res){
     var email = req.body.email;
     var password = req.body.password;
+
+    var query = "Select first_name, last_name, email, password from users where email='"+email+"' and verified="+1;
+    con.query(query, function(err, result){
+        if(err) throw err;
+        if(!result || Object.keys(result).length == 0){
+            res.end("Incorrect email");
+        }else{
+            if(bcrypt.compareSync(password,result[0].password)){
+                res.end(JSON.stringify(result[0]));
+            }else{
+                res.end("Incorrect password");
+            }
+        }
+    });
 });
 
+
+app.get('/:email/:token', function(req, res){
+    var email = req.params.email;
+    var token = req.params.token;
+    
+});
 
 function verifyCredentials(firstName, lastName, email, password){
     if(!firstName || !lastName || !email || !password ){
@@ -70,6 +124,6 @@ function verifyCredentials(firstName, lastName, email, password){
 }
 
 
-app.listen(8080,function(){
-    console.log('App is running on port 8080');
+app.listen(port,function(){
+    console.log(`App is running on port ${port}`);
 });
